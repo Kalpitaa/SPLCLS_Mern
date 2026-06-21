a46b9678ed089404dff56239a812688857534758
kalpitaa <mgkalpitaa@gmail.com>
2026-06-21 19:47:49 +0530

    June_19 task3 done

---
+++ June_19/Task3/controllers/userController.js
@@ -0,0 +1,39 @@
+export const registerUser = (req, res) => {
+  const { name } = req.body;
+
+  if (name === "Ravi") {
+    return res.status(200).json({
+      success: true,
+      message: "Registration Successful"
+    });
+  }
+
+  return res.status(400).json({
+    success: false,
+    message: "Invalid User"
+  });
+};
+
+export const loginUser = (req, res) => {
+  const { email, password } = req.body;
+
+  if (email === "admin@gmail.com" && password === "12345") {
+    return res.status(200).json({
+      success: true,
+      message: "Login Successful"
+    });
+  }
+
+  return res.status(401).json({
+    success: false,
+    message: "Invalid Credentials"
+  });
+};
+
+export const getUserById = (req, res) => {
+  res.status(200).json({
+    success: true,
+    id: req.params.id,
+    message: "User Found"
+  });
+};
