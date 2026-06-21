ed82e6badb86fdde0498ff8959020742e9d925e1
kalpitaa <mgkalpitaa@gmail.com>
2026-06-21 19:29:28 +0530

    June_19 Task2 done

---
+++ June_19/Task2/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import userRoutes from "./routes/userRoutes.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
